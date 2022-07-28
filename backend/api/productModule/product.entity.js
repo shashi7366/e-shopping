e3feb74598bf6a8711740f84bb1d2979e75dfcb9
shashi <shashikantsingh7366@gmail.com>
2022-07-28 11:44:07 +0530

    minor changes made to product api

--- backend/api/productModule/product.entity.js
+++ backend/api/productModule/product.entity.js
@@ -22,7 +22,7 @@ const productSchema = new mongoose.Schema({
         {
         public_id:{
             type:String,
-            required:true
+            default:"image not available"
         },
         url:{
             type:String,
